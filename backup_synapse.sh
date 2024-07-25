94923ed73d78825eb7ed38cb92c2f496d1673049
Cross <cross@dill.moe>
2024-07-25 07:06:45 -0400

    Update backup_synapse.sh

--- backup_synapse.sh
+++ backup_synapse.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
 
-# Set backup directory to a directory outside of the Synapse dir
 BACKUP_DIR="/home/cross/HDD/synapse_backups"
 
-# Create backup directory if it doesn't exist
 sudo mkdir -p "$BACKUP_DIR"
 
-# Get current date and time
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# Check if Synapse stack is running
-if ! docker compose -f /home/cross/synapse/docker-compose.yml ps | grep -q "Up"; then
-  echo "Synapse stack is not running. Skipping backup."
-  exit 1
-fi
+cd /home/cross/synapse
 
-# Create a snapshot of the storage directory using rsync
-sudo rsync -a --delete --link-dest=/home/cross/synapse/storage /home/cross/synapse/storage/ /home/cross/synapse/storage.snapshot/
+sudo docker compose down
 
-# Database backups
-sudo docker exec -t maindb pg_dump -U postgres -c -C synapse > /home/cross/synapse/maindb_backup.sql &
-sudo docker exec -t syncdb pg_dump -U postgres -c -C syncv3 > /home/cross/synapse/syncdb_backup.sql &
+cd /home/cross
 
-# Wait for all background jobs to complete
-wait
+sudo tar -czvpf "$BACKUP_DIR/synapse_backup_$DATE.tgz" /home/cross/synapse
 
-# Combine backups and save as a dated.tgz file in the backup directory
-sudo tar -czvpf "$BACKUP_DIR/synapse_backup_$DATE.tgz" \
-    /home/cross/synapse/data \
-    /home/cross/synapse/maindb_backup.sql \
-    /home/cross/synapse/syncdb_backup.sql \
-    /home/cross/synapse/docker-compose.yml \
-    /home/cross/synapse/storage.snapshot/
+cd /home/cross/synapse
 
-# Cleanup leftover.sql files and snapshot directory
-sudo rm /home/cross/synapse/*.sql
-sudo rm -rf /home/cross/synapse/storage.snapshot/
+sudo docker compose up -d
