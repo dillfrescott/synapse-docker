231ae8bf19486db62ee833be515a9f0a8cce6684
Cross <cross@dill.moe>
2024-07-25 07:37:38 -0400

    Update backup_synapse.sh

--- backup_synapse.sh
+++ backup_synapse.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-BACKUP_DIR="/home/cross/HDD/synapse_backups"
-
-sudo mkdir -p "$BACKUP_DIR"
-
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-
 cd /home/cross/synapse
 
 sudo docker compose down
 
 cd /home/cross
 
-sudo tar -czvpf "$BACKUP_DIR/synapse_backup_$DATE.tgz" /home/cross/synapse
+sudo RESTIC_PASSWORD=<password> restic -r /home/cross/HDD/restic_synapse backup /home/cross/synapse
 
 cd /home/cross/synapse
 
