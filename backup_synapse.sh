75f63a2376413f4cbd0559d80bb9cde7121227d4
Cross <cross@dill.moe>
2024-07-26 14:48:25 -0400

    Update backup_synapse.sh

--- backup_synapse.sh
+++ backup_synapse.sh
@@ -17,3 +17,6 @@ sudo tar -czvpf "$BACKUP_DIR/synapse_backup_$DATE.tgz" /home/cross/synapse
 cd /home/cross/synapse
 
 sudo docker compose up -d
+
+# Clean up old backups, keeping only the last 3 days
+sudo find "$BACKUP_DIR" -type f -name "synapse_backup_*.tgz" -mtime +3 -exec rm -f {} \;
